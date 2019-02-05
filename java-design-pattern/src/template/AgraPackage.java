3b2055eaae925fcb8dd75b339ba8ea5b9bdcd2c5
GESDGODC\ManishJ <manish.jain@ge.com>
2019-02-05 19:24:54 +0530

    template design pattern code commit

---
+++ java-design-pattern/src/template/AgraPackage.java
@@ -0,0 +1,21 @@
+package template;
+
+public class AgraPackage {
+
+    public void doComingTransport() {
+        System.out.println("The turists are comming by Train ...");
+	}
+	public void doDayA() {
+	        System.out.println("The turists are visiting the TajMahal ...");
+	}
+	public void doDayB() {
+	        System.out.println("The turists are going to the Red Fort ...");
+	}
+	public void doDayC() {
+	        System.out.println("The turists are going to Fatahpur Sikari ...");
+	}
+	public void doReturningTransport() {
+	        System.out.println("The turists are going home by Train ...");
+	}
+
+}
---
+++ java-design-pattern/src/template/GoaPackage.java
@@ -0,0 +1,21 @@
+package template;
+
+public class GoaPackage {
+
+    public void doComingTransport() {
+        System.out.println("The turists are comming by Air ...");
+	}
+	public void doDayA() {
+	        System.out.println("The turists are visiting the North Goa ...");
+	}
+	public void doDayB() {
+	        System.out.println("The turists are going to the South Goa ...");
+	}
+	public void doDayC() {
+	        System.out.println("The turists are going to Casino ...");
+	}
+	public void doReturningTransport() {
+	        System.out.println("The turists are going home by Air ...");
+	}
+
+}
